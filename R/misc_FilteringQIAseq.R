389a775fb4526ffa1ffc72eb6f8fd4a3ea3d52ca
J0bbie <j.vanriet@erasmusmc.nl>
2021-04-29 16:37:35 +0200

    Sync

--- R/misc_FilteringQIAseq.R
+++ R/misc_FilteringQIAseq.R
@@ -1,5 +1,5 @@
 # Author:                      Job van Riet
-# Date:                        14-01-2021
+# Date:                        29-04-2021
 # Function:                    Import unfiltered QIAseq mutations, add the CPCT-02 mPRAD incidence and perform filtering.
 
 
@@ -12,75 +12,100 @@ library(ggplot2)
 
 # Import data -------------------------------------------------------------
 
-# Load the CPCT-02 DR-071 cohort.
-load('/mnt/data2/hartwig/DR71/Oct2020/RData/data.Cohort.RData')
-
 # Import unfiltered QIAseq mutations.
-unfilteredMuts <- readxl::read_xlsx('Misc./QIAseq_UnfilteredMuts.xlsx', trim_ws = T)
+unfilteredMuts <- dplyr::bind_rows(pbapply::pblapply(list.files('/mnt/data2/hartwig/DR71/Misc/Annotated/', pattern = 'tsv.gz$', full.names = T), function(x){
 
-# Remove mutations with a known location in hg19.
-unfilteredMuts <- unfilteredMuts %>% dplyr::filter(`[1]LOC_hg19` != 'not found')
+    # Import.
+    data <- readr::read_tsv(x, guess_max = 299999, trim_ws = T)
+    data$lcode <- gsub('.ann.tsv.gz', '', basename(x))
 
+    # Clean-up columns.
+    colnames(data) <- gsub('# ', '', gsub('-|:', '', gsub(unique(data$lcode), '', colnames(data))))
 
-# Add Mut. Frequencies ----------------------------------------------------
+    # Filter non-standard chromosomes.
+    data <- data %>% dplyr::filter(`[1]CHROM` %in% c(1:22, 'X', 'Y'))
 
-# Convert mutations into GRanges to check overlap.
-muts.Gr <- GenomicRanges::sort(GenomicRanges::GRanges(seqnames = gsub(':.*', '', unfilteredMuts$`[1]LOC_hg19`), ranges = IRanges::IRanges(start = as.numeric(gsub('.*:', '', unfilteredMuts$`[1]LOC_hg19`)), end = as.numeric(gsub('.*:', '', unfilteredMuts$`[1]LOC_hg19`)))))
-muts.Gr$`[1]LOC_hg19` <- unfilteredMuts$`[1]LOC_hg19`
-muts.Gr <- unique(muts.Gr)
+    # Fix nomenclature.
+    data <- data %>% dplyr::mutate(`[1]CHROM` = paste0('chr', `[1]CHROM`))
 
-# Perform overlap.
-muts.CPCT <- IRanges::subsetByOverlaps(data.Cohort$somaticVariants, muts.Gr, type = 'within')
-muts.CPCT$LOC <- sprintf('%s:%s', seqnames(muts.CPCT), start(muts.CPCT))
+    # Add position-based identifier.
+    data <- data %>% dplyr::mutate(ID_Job = sprintf('%s.%s:%s>%s', `[1]CHROM`, `[2]POS`, `[4]REF`, `[5]ALT`))
 
-muts.CPCT <- tibble::as_tibble(muts.CPCT) %>%
-    dplyr::group_by(LOC) %>%
-    dplyr::summarise(totalCPCT = dplyr::n_distinct(sample)) %>%
-    dplyr::ungroup()
+    # Retrieve ALT AF.
+    data$`[28]consensus5AF_Job` <- unlist(lapply(strsplit(data$`[28]consensus5AF`, ','), function(x) as.numeric(x)[2]))
+    data$`[28]consensus5AF_Job` <- ifelse(is.infinite(data$`[28]consensus5AF_Job`), NA, data$`[28]consensus5AF_Job`)
 
-# Add back to QIAseq mutations.
-unfilteredMuts <- unfilteredMuts %>% dplyr::left_join(muts.CPCT, by = c('[1]LOC_hg19' = 'LOC'))
+    # Fix gene names.
+    data <- data %>% dplyr::mutate(`[17]ann_gene_name_Job` = gsub('\\|.*', '', `[17]ann_gene_name`))
+
+    # Fix annotations.
+    data <- data %>% dplyr::mutate(`[15]ann_annotation_Job` = gsub('\\|.*', '', `[15]ann_annotation`))
+
+    # Return cleaned data.
+    return(data)
+
+}, cl = 20))
 
 
 # Perform filtering. ------------------------------------------------------
 
-# Determine whether mutations overlap the panel-design.
-regions.panel <- c(rtracklayer::import.bed('Misc./QIAseq_DNA_panel.CDHS-25058Z-3602.primers-150bphg19.bed'), rtracklayer::import.bed('Misc./QIAseq_DNA_panel.CDHS-25058Z-3602.roihg19.bed'))
+## Determine whether mutations overlap the panel-design. ----
+
+regions.panel <- c(rtracklayer::import.bed('Misc./QIAseq_DNA_panel.CDHS-25058Z-3602.primers-150bphg38.bed'), rtracklayer::import.bed('Misc./QIAseq_DNA_panel.CDHS-25058Z-3602.roihg38.bed'))
+strand(regions.panel) <- '*'
+
 regions.panel <- GenomicRanges::reduce(regions.panel)
 
-overlappingPanel <- IRanges::subsetByOverlaps(muts.Gr, regions.panel, type = 'within')
+muts.Gr <- GenomicRanges::makeGRangesFromDataFrame(unfilteredMuts, keep.extra.columns = F, seqnames.field = '[1]CHROM', start.field = '[2]POS', end.field = '[2]POS')
+muts.Gr$ID_Job <- unfilteredMuts$ID_Job
+overlappingPanel <- IRanges::subsetByOverlaps(muts.Gr, regions.panel)
 
 # Add within panel information back to the table.
-unfilteredMuts$withinPanel <- unfilteredMuts$`[1]LOC_hg19` %in% overlappingPanel$`[1]LOC_hg19`
+unfilteredMuts$withinPanel <- unfilteredMuts$ID_Job %in% overlappingPanel$ID_Job
 
-unfilteredMuts <- unfilteredMuts %>%
-    dplyr::group_by(`[1]LOC_hg19`) %>%
-    dplyr::mutate(totalCaba = dplyr::n_distinct(lcode)) %>%
-    dplyr::ungroup()
 
-# Perform filtering.
+## Determine presence within PON ----
+
+HMF.PON <- VariantAnnotation::readVcfAsVRanges('/mnt/data2/hartwig/DR71/Misc/SageGermlinePon.98x.38.vcf.gz')
+HMF.PON.Filtered <- IRanges::subsetByOverlaps(HMF.PON, muts.Gr)
+HMF.PON.Filtered <- HMF.PON.Filtered[HMF.PON.Filtered$PON_COUNT >= 10 & HMF.PON.Filtered$PON_MAX >= 5]
+HMF.PON.Filtered$ID_Job <- sprintf('%s.%s:%s>%s', seqnames(HMF.PON.Filtered), start(HMF.PON.Filtered), VariantAnnotation::ref(HMF.PON.Filtered), VariantAnnotation::alt(HMF.PON.Filtered))
+
+# Add to table.
+unfilteredMuts$inPON <- unfilteredMuts$ID_Job %in% HMF.PON.Filtered$ID_Job
+
+
+## Perform filtering. ----
+
 filteredMuts <- unfilteredMuts %>%
     dplyr::filter(
         # Filter mutations within panel-design.
-        (withinPanel | totalCPCT >= 5),
-        # Filter on incidence within CABA-V7 samples.
-        (totalCaba <= 30 | totalCPCT >= 5),
-        # Filter out SNPs.
-        (SNP == 'n' | totalCPCT >= 5),
+        withinPanel,
+        # Filter on HMF PON.
+        !inPON,
         # Filter on DP.
-        (`[35]consensus-5:DP` >= 100 | totalCPCT >= 5),
+        `[29]consensus5DP` >= 100,
         # Filter on min. VAF.
-        ((as.numeric(`[34]consensus-5:AF`) >= 0.05 & !is.na(as.numeric(`[34]consensus-5:AF`))) | totalCPCT >= 5),
+        (`[28]consensus5AF_Job` >= 0.05 & !is.na(`[28]consensus5AF_Job`)),
         # Filter on max. VAF.
-        ((as.numeric(`[34]consensus-5:AF`) <= 0.95 & !is.na(as.numeric(`[34]consensus-5:AF`))) | totalCPCT >= 5),
+        (`[28]consensus5AF_Job` <= 0.95 & !is.na(`[28]consensus5AF_Job`)),
         # Filter on coding.
-        `[19]ann_hgvs_p` != '.'
+        `[18]ann_hgvs_p` != '.',
+        # Only take known COSMIC mutations.
+        grepl('COS', `[3]ID`)
     ) %>%
-    dplyr::group_by(`[1]LOC_hg19`) %>%
+    # Filter on incidence within CABA-V7 samples.
+    dplyr::group_by(ID_Job) %>%
     dplyr::mutate(totalCaba = dplyr::n_distinct(lcode)) %>%
-    dplyr::ungroup()
+    dplyr::ungroup() %>%
+    dplyr::filter(totalCaba <= 30)
 
 
 # Output mutations --------------------------------------------------------
+
+filteredMuts <- filteredMuts %>%
+    dplyr::group_by(ID_Job) %>%
+    dplyr::mutate(totalCaba = dplyr::n_distinct(lcode)) %>%
+    dplyr::ungroup()
 
 write.table(filteredMuts, file = 'asd.txt', quote = F, sep = '\t', row.names = F)
